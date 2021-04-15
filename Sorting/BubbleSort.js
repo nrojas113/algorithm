c4ed645021f7fa647a88f96ace102b5dae8c62f0
Natalie Rojas <natalie.rojas113@gmail.com>
2021-04-14 21:55:03 -0400

    Bubble Sort

--- Sorting/BubbleSort.js
+++ Sorting/BubbleSort.js
@@ -2,11 +2,15 @@
 A sorting algorithm where the largest values bubble up to the top
 
 The bubble sort method starts at the beginnign of an
-unsorted array and 'bubbles up' unsorted values toward the end, iterating through the aray until it is completely sorted.
+unsorted array and 'bubbles up' unsorted values toward the end,
+iterating through the aray until it is completely sorted.
 Through each iteration, it compaires adjacent items and
-swapp them if they are out of order.
+swap them if they are out of order.
 The method continues looping through the array until no
-swaps occur at whihc point the array is sorted.
+swaps occur at which point the array is sorted.
+
+input : [5,3,4,1,2]
+output : [1,2,3,4,5]
 
 1st loop
 [5,3,4,1,2]
@@ -18,7 +22,6 @@ swaps occur at whihc point the array is sorted.
 2nd loop
 [3,4,1,2,5]
 [3,1,4,2,5]
-[3,1,2,4,5]
 [3,1,2,4,5] <=2nd to last element is in place (bubbled)
 
 3rd loop
@@ -29,26 +32,46 @@ swaps occur at whihc point the array is sorted.
 Approach:
 1. loop over array from end to beginning
 2. start inner loop from beginning until i - 1
-3. if current num in the index is larger then next num, swap those two
+3. if current num in the index is less then previous num, swap those two
 4. Repeat the process until all are sorted
 5. Optimize by adding stopping condition when no swaps are happening
 */
 
 function bubbleSort(arr) {
   let noSwaps;
-
+  //keep track of which iteration we are on
   for (let i = 0; i < arr.length; i++) {
+    //in the beginning, we assume that there are no swaps every time through
+    //reset the noSwaps status to true everytime
     noSwaps = true;
     for (let j = 1; j < arr.length - i; j++) {
-      if (arr[j] > arr[j + 1]) {
-        //swap current num & next num (move large num to later)
+      //compare second item to previous item
+      if (arr[j] < arr[j - 1]) {
+        //swap current num & previous num (move large num to later)
         let temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
+        arr[j] = arr[j - 1];
+        arr[j - 1] = temp;
+        //another way to swap
+        //[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+
+        //if this loop was triggered, swap happened so set to false.
+        //this will continue to be false as long as swaps are happening
         noSwaps = false;
       }
     }
+    //if swaps happened, noSwap is false so continue the loop.
+    //if no swaps happened, noSwap is true (didn't change) so break out of the whole loop
     if (noSwaps) break;
   }
   return arr;
 }
+
+/*
+Time complexity :
+best case - O(n)
+worst case - O(n^2)
+Because we have nested loop, and we are making roughly making a comparison, for each item in that array, we are making N number of comparisons.
+If data is nearly sorted or sorted already, then it will be O(n) - linear time
+If we know that the data is nearly sorted, then bubble sort could be good.
+
+*/
